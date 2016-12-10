391d3c8488506e595b3cdce98d8305e70a043356
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-10 04:29:48 +0000

    Add static route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
